cbfa69b97fef786f487cd42ca4b5f183045c117c
Raquel Martiniano <raquelmartiniano14@gmail.com>
2026-04-14 16:22:39 +0000

    feito o teste, mas falta testar

--- Atividade/TesteFilaReversao.java
+++ Atividade/TesteFilaReversao.java
@@ -1,8 +1,19 @@
 package Atividade;
 public class TesteFilaReversao{
     public static void main(String[] args){
+        FilaArray_Reversao execute = new FilaArray_Reversao(5);
 
-        
+        System.out.println(execute.size());
+        execute.enqueue(2);
+        execute.enqueue(3);
+        execute.enqueue(4);
+        execute.enqueue(5);
+
+        System.out.println(execute.size());
+        execute.botao();
+
+        execute.dequeue();
+        execute.dequeue();        
 
 
     } 
